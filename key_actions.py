3c98463b4e08346ee304c0e04274e8375b1518fd
phelippemichel <phelippe.michel@gmail.com>
2024-02-02 16:32:45 -0300

    feat: add key actions

---
+++ key_actions.py
@@ -0,0 +1,27 @@
+import ctypes
+import time
+from key_codes import *
+
+hwnd = ctypes.windll.user32.GetForegroundWindow.os.getenv("WINDOW_NAME")
+
+def send_message_keyboard(hwnd, key_code):
+    ctypes.windll.user32.PostMessageW(hwnd, WM_KEYDOWN, key_code, 0)
+    time.sleep(0.1)
+    ctypes.windll.user32.PostMessageW(hwnd, WM_KEYUP, key_code, 0)
+
+def moveToMouse(hwnd, x, y):
+    x = int(x)
+    y = int(y)
+    ctypes.windll.user32.PostMessageW(hwnd, WM_MOUSEMOVE, MK_LBUTTON)
+
+def clickMouse(hwnd, x, y, button="Mleft"):
+    x = int(x)
+    y = int(y)
+    if button == "Mleft":
+        ctypes.windll.user32.PostMessageW(hwnd, WM_LBUTTONDOWN, 1)
+        time.sleep(0.01)
+        ctypes.windll.user32.PostMessageW(hwnd, WM_LBUTTONUP, 0)
+        return
+    ctypes.windll.user32.PostMessageW(hwnd, WM_RBUTTONDOWN, 1)
+    time.sleep(0.01)
+    ctypes.windll.user32.PostMessageW(hwnd, WM_RBUTTONUP, 0)
